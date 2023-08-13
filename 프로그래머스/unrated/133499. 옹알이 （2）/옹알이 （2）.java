ff89cf55e6479a1504608d4751653cfbfdd82246
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2023-08-13 15:52:54 +0900

    [unrated] Title: 옹알이 (2), Time: 0.82 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+
+        for (String str : babbling) {
+            if (str.contains("ayaaya") || str.contains("mama") || str.contains("woowoo") || str.contains("yeye")) {
+                continue;
+            }
+
+            str = str.replace("aya", " ");
+            str = str.replace("ma", " ");
+            str = str.replace("woo", " ");
+            str = str.replace("ye", " ");
+            str = str.replace(" ", "");
+            if (str.length() == 0) {
+                answer ++;
+            }
+        }
+
+
+        return answer;
+    }
+}
