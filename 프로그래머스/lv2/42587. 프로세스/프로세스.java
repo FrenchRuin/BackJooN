21c3b1523657b7400b4c469b6573390a7eec7701
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2023-09-18 00:09:43 +0900

    [level 2] Title: 프로세스, Time: 0.65 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (Integer integer : priorities) {
+            priorityQueue.add(integer);
+        }
+
+        while (!priorityQueue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+
+                if (priorities[i] == priorityQueue.peek()) {
+                    priorityQueue.poll();
+                    answer++;
+
+                    if (i == location) {
+                        return answer;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
