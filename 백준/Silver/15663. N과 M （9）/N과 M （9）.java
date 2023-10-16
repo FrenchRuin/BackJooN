175378ebedc0fec7b3210789afe90cb2667b01dd
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2023-10-16 23:22:22 +0900

    [Silver II] Title: N과 M (9), Time: 288 ms, Memory: 22404 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+import java.lang.reflect.Array;
+
+public class Main {
+
+  /*
+   * 
+   */
+  static int M;
+  static int N;
+  static int[] arr;
+  static int[] answer;
+  static boolean[] visited;
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // StringTokenizer st = new StringTokenizer(br.readLine());
+
+    // N = Integer.parseInt(st.nextToken()); // ex) 3
+    // M = Integer.parseInt(st.nextToken()); // ex) 1
+
+    Scanner sc = new Scanner(System.in);
+
+    N = sc.nextInt();
+    M = sc.nextInt();
+
+    arr = new int[N];
+    answer = new int[M];
+    visited = new boolean[N];
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    Arrays.sort(arr);
+
+    recur(0);
+
+    System.out.println(sb);
+  }
+
+  static void recur(int depth) {
+    if (depth == M) {
+      for (int i : answer) {
+        sb.append(i).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+    int before = 0;
+    for (int i = 0; i < N; i++) {
+
+      if (visited[i])
+        continue;
+
+      if (before != arr[i]) {
+        visited[i] = true;
+        answer[depth] = arr[i];
+        before = arr[i];
+        recur(depth + 1);
+        visited[i] = false;
+      }
+    }
+  }
+}
