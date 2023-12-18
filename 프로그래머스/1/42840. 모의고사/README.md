ceba52806fba9b68967897b424fd9f2d98618bba
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2023-12-18 21:28:11 +0900

    [level 1] Title: 모의고사, Time: 5.40 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 89.5 MB, 시간: 3.51 ms
+메모리: 10.3 MB, 시간: 5.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 22:38:57
+2023년 12월 1일 21:28:10
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,22 @@
+def solution(answers):
+    
+    first  = [1,2,3,4,5]
+    second = [2,1,2,3,2,4,2,5]
+    third  = [3,3,1,1,2,2,4,4,5,5]
+    
+    result = []
+    scores = [0,0,0]
+    
+    for idx, answer in enumerate(answers) :
+        if answer == first[idx % len(first)] : 
+            scores[0] += 1
+        if answer == second[idx % len(second)] : 
+            scores[1] += 1
+        if answer == third[idx % len(third)] : 
+            scores[2] += 1
+    
+    for idx, score in enumerate(scores) :
+        if score == max(scores) :
+            result.append(idx + 1)
+            
+    return result
