092d42b7a828697f1f914186161662f08a101393
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-02-19 01:05:28 +0900

    [Bronze II] Title: 초6 수학, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2702. 초6 수학/초6 수학.py
@@ -0,0 +1,20 @@
+
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+
+    return x
+
+
+def lcm(x, y):
+    return (x * y) // gcd(x, y)
+
+
+for _ in range(T):
+    a, b = map(int, input().split())
+    print(lcm(a,b), gcd(a,b))
