e83d9c1d6ec6e7c44bf31220b203ca643491597a
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-02-21 21:53:45 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,12 +1,12 @@
 def solution(brown, yellow):
     answer = []
     
-    xy = brown + yellow
-    for x in range(1 , xy + 1) :
+    xy = brown + yellow 
+    for x in range(1, xy + 1) :
         if (xy / x) % 1 == 0 :
-            y = xy / x
-            if x >= y :
-                if 2*x + 2*y == brown + 4 :
-                    answer = [x,y]
+            y = xy/x
+            if x >= y and x*2 + y*2 == brown + 4:
+                answer = [x,y]
+                
                     
     return answer
