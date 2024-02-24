82155db415c0e6505743f879acbdd771dfbf79cf
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-02-24 12:02:37 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -2,20 +2,21 @@ from collections import deque
 
 def solution(tickets):
     answer = []
+    
     visited = [0] * len(tickets)
     
     def dfs(airport, path) :
         
         if len(path) == len(tickets) + 1 :
             answer.append(path)
-            return 
+            return
         
         for idx, ticket in enumerate(tickets) :
-            if airport == ticket[0] and not visited[idx] :
+            if ticket[0] == airport and not visited[idx] :
                 visited[idx] = 1
-                dfs(ticket[1], path+[ticket[1]])
+                dfs(ticket[1], path + [ticket[1]])
                 visited[idx] = 0
-                
-    dfs("ICN", ["ICN"])
+        
+    dfs("ICN",["ICN"])
     answer.sort()
     return answer[0]
