db3b70bb01870a1b4c9dac7a7fb35d44bc83ff72
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-02-24 13:46:41 +0900

    [level 2] Title: 소수 찾기, Time: 0.30 ms, Memory: 7.73 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -8,20 +8,18 @@ def solution(numbers) :
     for i in range(1, len(numbers) + 1) :
         nums += list(permutations(numbers, i))
     
-    new_nums = [int(''.join(x)) for x in nums ]
+    new_nums = [int(''.join(x)) for x in nums]
     new_nums = set(new_nums)
-
     answer = []
     for n in new_nums :
         if is_prime(n) :
             answer.append(n)
-            
         
     return len(answer) 
 
 
 def is_prime(n) :
-    if n<2 :
+    if n < 2 :
         return False
     
     for i in range(2, int(math.sqrt(n)) + 1) :
