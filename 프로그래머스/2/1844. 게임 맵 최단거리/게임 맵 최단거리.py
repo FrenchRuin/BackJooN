e0105b23f8665ca9589aa7e1cf3a1a4da049d204
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-02-24 11:46:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,39 +1,30 @@
 from collections import deque
 
-
-
 def solution(maps):
     answer = 0
-    answer = bfs(maps)
-    
-    if answer == 0 :
-        return -1
-    
-    return answer
-
-def bfs(maps) :
+    w = len(maps[0])
+    h = len(maps)
+    visited = [[0] * (w + 1) for _ in range(h + 1)]
     
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
-    
-    n = len(maps[0])
-    m = len(maps)
-    visited = [[1] * (n + 1) for _ in range(m + 1)]
     queue = deque()
     queue.append((0,0))
-    cnt = 0
-    while queue : 
+    visited[0][0] = 1
+    while queue :
         x,y = queue.popleft()
-        if x == m - 1 and y == n - 1 :
-            cnt = visited[x][y]
+        
+        if x == h - 1 and y == w - 1 :
+            answer = visited[x][y]
             break
-            
+        
         for i in range(4) :
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if 0<=nx<m and 0<=ny<n :
-                if visited[nx][ny] == 1 and maps[nx][ny] == 1 :
+            if 0<=nx<h and 0<=ny<w :
+                if not visited[nx][ny] and maps[nx][ny] == 1 :
                     visited[nx][ny] = visited[x][y] + 1
                     queue.append((nx,ny))
-    return cnt
+                    
+    return answer if answer >0 else -1
