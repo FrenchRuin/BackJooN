a1568197168050c451995fe9eb76d7c4d2f2ebb8
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-02-26 20:59:26 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,15 +1,16 @@
 def solution(n, words):
-    answer = [1,1]
-    strList = []
-    chk = True
-    strList.append(words[0])
+    answer = []
+    already = set()
+    for i in range(len(words)) :
+        if len(already) == 0 :
+            already.add(words[i])
+        elif words[i] in already :
+            return [ (i % n) + 1, (i // n) + 1]
+        else:
+            if words[i-1][-1] == words[i][0] :
+                already.add(words[i])
+            else:
+                return [ (i % n) + 1, (i // n) + 1]
+            
     
-    for i in range(1, len(words)) :
-        if((words[i][:1] != words[i-1][-1]) or (strList.count(words[i]) > 0)) :
-            answer[0] = (i % n) + 1
-            answer[1] = (i // n) + 1
-            chk = False
-            break
-        strList.append(words[i])
-    
-    return answer if chk == False else [0,0]
+    return [0,0]
