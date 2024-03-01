e507c5de955cd6b1e39682e700f77e25be1ad032
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-03-01 14:50:42 +0900

    [Gold V] Title: 귀찮은 해강이, Time: 264 ms, Memory: 45892 KB -BaekjoonHub

--- 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.py
+++ 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.py
@@ -1,66 +1,39 @@
-"""
-UNION Find 기초 문제다.
-
-연결되어있는 건물이 존재하고,
-수업시간표가 존재하면,
-
-건물밖을 나와야한다.
-그렇다면 수업을 듣기위해 최소 나와야하는 횟수를 구한다.
-
-건물 5 / 연결되어있는건물 3 케이스
-1번 3번
-2번 5번
-3번 4번 건물 연결
-
-연결되어있는 건물들을 같은 집합으로 본다.
-1,3,4
-
-2,5
-
---
-1 2 3 5 4 수업 시간표라고 가정한다면
-1번건물에서 2번건물로 가는데
-1번 건물과 2번건물이 다른집합일때 카운트를 플러스한다.
-"""
-
 import sys
 
 input = sys.stdin.readline
 
+N,M = map(int,input().split())
 
-def union_find(V):
-    if V != buildings[V]:
-        buildings[V] = union_find(buildings[V])
-    return buildings[V]
+buildings = [0] * (N + 1)
 
 
-def union(A, B):
-    A = union_find(A)
-    B = union_find(B)
+def findParent(V) :
+  if buildings[V] != V :
+    buildings[V] = findParent(buildings[V])
+    
+  return buildings[V]
 
-    if A > B:
-        buildings[A] = B
-    else:
-        buildings[B] = A
+def union(A,B) :
+  A = findParent(A)
+  B = findParent(B)
+  if A> B :
+    buildings[A] = B
+  else:
+    buildings[B] = A
 
 
-N, M = map(int, input().split())  # N -> 건물갯수 / M -> 연결되어있는 건물 케이스
+for i in range(1,N + 1) :
+  buildings[i] = i
 
-buildings = [0] * (N + 1)  # 건물들
+for _ in range(M) :
+  A,B = map(int,input().split())
+  union(A,B)
 
-for i in range(1, N + 1):  # 1,2,3... 입력
-    buildings[i] = i
+plans = list(map(int,input().split()))
 
-# 연결되어있는 건물 입력
-for i in range(M):
-    A, B = map(int, input().split())
-    union(A, B)
-
-# 시간표
-timeTable = list(map(int, input().split()))
 answer = 0
-for i in range(1, len(timeTable)):
-    if union_find(timeTable[i - 1]) != union_find(timeTable[i]):
-        answer += 1
+for i in range(1, len(plans)) :
+  if findParent(plans[i-1]) != findParent(plans[i]) :
+    answer += 1
 
 print(answer)
