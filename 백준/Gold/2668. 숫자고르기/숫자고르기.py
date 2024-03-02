540cc83942657bb451e2921b483cefe7bb23fb35
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-03-02 13:25:26 +0900

    [Gold V] Title: 숫자고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,25 +1,30 @@
 import sys
 
 input = sys.stdin.readline
-def dfs(v, i):
-    visited[v] = 1
-    num = graph[v]
-    if not visited[num]:
-        dfs(num, i)
-    elif visited[num] and num == i:
-        result.append(num)
-
 
 N = int(input())
-graph = [0] + [int(input()) for _ in range(N)]
+
+graph = [0]
+for _ in range(N):
+  graph.append(int(input()))
 
 result = []
 
+
+def dfs(V, i):
+  visited[V] = 1
+  num = graph[V]
+  if not visited[num]:
+    dfs(num, i)
+  elif visited[num] and num == i:
+    result.append(num)
+
+
 for i in range(1, N + 1):
-    visited = [0] * (N + 1)
-    dfs(i, i)
+  visited = [0] * (N + 1)
+  dfs(i, i)
 
 result.sort()
 print(len(result))
 for r in result:
-    print(r)
+  print(r)
