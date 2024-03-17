fa330976927df2bfcfb0f791aaa317c89dbf7498
Dev_Lee <100774179+FrenchRuin@users.noreply.github.com>
2024-03-17 16:35:18 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 100 ms, Memory: 35664 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,33 +1,34 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
- 
-a,b = map(int,input().split())
-limit = 100001
-time = [0]*limit
- 
-def bfs(x,y):
-    q = deque()
-    if x == 0 :
-        q.append(1)
-    else :
-        q.append(x)
+
+N,K = map(int,input().split())
+
+visited = [0] * 100001
+
+
+
+def bfs(V) :
+  q = deque()
+  if V == 0 :
+    q.append(1)
+  else:
+    q.append(V)
+  while q :
+    V = q.popleft()
+
+    if V == K :
+      return visited[V]
     
-    while q:
-        x = q.popleft()
-        if y == x :
-            return time[x]
-        
-        for nx in (x-1,x+1,x*2):
-            if 0 <= nx < limit and time[nx]==0:
-                if nx == 2*x :
-                    time[nx] = time[x]
-                    q.appendleft(nx)
-                else : 
-                    time[nx] = time[x] + 1
-                    q.append(nx)
- 
-if a==0 :
-    print(bfs(a,b)+1)
-else :
-    print(bfs(a,b))
+    for i in (V - 1, V + 1, V * 2) :
+      if 0<= i < 100001 and not visited[i] :
+        if i == V*2 :
+          visited[i] = visited[V]
+          q.appendleft(i)
+        else:
+          visited[i] = visited[V] + 1
+          q.append(i)
+if N == 0 :
+  print(bfs(N) + 1)
+else:
+  print(bfs(N))
